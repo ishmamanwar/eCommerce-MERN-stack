bee9d044c03b63f201e3a12ed9f19c29d50b22ae
ishmamanwar <ishmam.anwar96@gmail.com>
2021-04-27 14:13:54 -0600

    routing for product controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const { getAllProducts, getProductById } = require('../controller/productController');
+
 //@desc     GET all products from db
 //@route    GET /api/products
 //@access   Public
-router.get('/', (req, res) => {
-
-})
+router.get('/', getAllProducts)
 
 //@desc     GET a product by id from db
 //@route    GET /api/products/:id
 //@access   Public
-router.get('/:id', (req, res) => {
-    
-})
-
+router.get('/:id', getProductById)
 module.exports = router;
